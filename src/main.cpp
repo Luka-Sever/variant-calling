3c26117140fb21c49a1a390528c90b2878f8b12f
Luka Sever <luka.sever20@gmail.com>
2026-06-05 21:04:20 +0200

    Dodan unos argumenta i kreiranje datoteke

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,25 @@
 #include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
 #include "fasta_reader.hpp"
 #include "sam_parser.hpp"
 #include "cigar_parser.hpp"
 #include "variant_caller.hpp"
 #include "types.hpp"
-#include <vector>
 
-int main(){
-    std::string ref_path = "../data/lambda.fasta";
-    std::string sam_path = "../data/aligment.sam";
+using namespace std::string_literals;
+
+int main(int argc, char* argv[]){
+    // početni argumenti
+    if (argc < 3) {
+        std::cout << "Greška! Uporaba: " << argv[0] << " <putanja FASTA datoteke> <putanja očitanja> <putanja za spremanje>\n";
+        exit(1); 
+    }
+    std::string ref_path(argv[1]);
+    std::string sam_path(argv[2]);
+
+    std::ofstream output(argv[3]);
 
     std::string reference = read_fasta(ref_path);
 
@@ -20,7 +31,34 @@ int main(){
         allVariants.insert(allVariants.end(), v.begin(), v.end());
     }
 
-    
     // traženje mutacije i određivanje pogreške
     std::vector<Mutation> mutations = variant_caller(records, reference);
+
+    // psianje u CSV formatu
+    std::string mutations_type[3] = {"X", "I", "D"};
+    for (const auto& mutation : mutations) {
+        std::string line;
+
+        // stvaranje CSV reda
+        line.append(mutations_type[mutation.type] + ",");
+        line.append(std::to_string(mutation.pos));
+        
+        // određivanje vrste mutacije
+        switch (mutation.type) {
+            case MutationType::SUBSTITUTION:
+                line.append(","s + mutation.alt_base);
+                break;
+
+            case MutationType::INSERTION:
+                line.append(","s + mutation.alt_base);
+                break;
+            
+            case MutationType::DELETION:
+                break;
+        }
+        
+        output << line << "\n";
+
+    }
+    output.close();
 }
