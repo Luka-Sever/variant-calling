d37d7adc4dd92775b0ec9d0e0e1dd60938f78618
Ivan Dundović <ivan.dundovic@fer.hr>
2026-06-07 19:39:35 +0200

    translate from croatian to english

--- src/fasta_reader.hpp
+++ src/fasta_reader.hpp
@@ -5,25 +5,19 @@
 #include <string>
 
 /*
-    TO DOO : 
-    1. Napraviti funkciju koja cita fasta file te sprema genom kao jedan string kako bi ga mogli koristiti za usporedbu :)
+    TODO:
+    1. Create a function that reads a FASTA file and stores the genome as a single string so it can be used for comparison :)
 
-    DOVRŠENO : DA
+    COMPLETED: YES
 */
+
 /*
-    Detaljniji opis funkcije:
-    Funkcija prima putanju do fajl (path). Kreiramo varijablu file tipa infstream (input file stream). (linija 25)
-    Zatim provjeravamo jesmo li uspijeli otvoriti fajl ako da nastavljamo inače bacamo grešku. (linija 26)
-    Kad smo prošli if petlju nastavljamo tako što stvaramo string genom i line. Genom koristimo kako bi u njega spremili
-    cijeli genom, dok line koristimo kako bi u nju stavljali liniju fajla koje ćemo dodavati u genom. (linija 27)
-    Pomću getline(file, line) uzimamo liniju po liniju i to radimo u while petlji sve dok nismo ostali bez linija. (linija 28)
-    Koristimo if provjeru ako fajl zapocinje s > to znaci da je header i ne treba nam u genomu pa ga ne dodajemo. (linija 28)
-    I na kraju veaćamo genom. (linija 30)
+    Function takes a file path, skips headers (">") and returns a concatenated string.
+    Written by: Ivan Dundović
 */
-
 std::string read_fasta(std::string path){
     std::ifstream file(path);
-    if(!file.is_open()) throw std::runtime_error("Ne mogu otvoriti fajl: " + path);
+    if(!file.is_open()) throw std::runtime_error("Cannot open the file: " + path);
     std::string genom, line;
     while (std::getline(file, line)) if(!line.empty() && line[0] != '>') genom += line;
 
