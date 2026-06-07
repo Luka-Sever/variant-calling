dac83d1871abb78e11f7342421ff33b3fb971c6b
Ivan Dundović <ivan.dundovic@fer.hr>
2026-06-07 19:47:53 +0200

    sam_parser.hpp to english

--- src/sam_parser.hpp
+++ src/sam_parser.hpp
@@ -12,43 +12,39 @@
 #include "types.hpp"
 
 /**
- * Pretvaramo sam datoteku u bam datoteku da samnjimo
- * potrošenu memoriju
+ * We convert the SAM file into a BAM file to reduce memory usage.
+ * Written by: Luka Sever
  */
-
 void convert_sam_to_bam(const char* inputFile, const char* outputFile) {
     /*
-    
-        Pokušavamo čitati input file i spremati
-        u output file
-    
+        We attempt to read the input file and save it to the output file.
     */
     samFile* file = sam_open(inputFile, "r");
-    if (!file) std::cout << "Ne mogu otvoriti datoteku.\n";
+    if (!file) std::cout << "Cannot open the file.\n";
 
     sam_hdr_t *header = sam_hdr_read(file);
-    if (!header) std::cout << "Ne mogu pročitati header.\n";
+    if (!header) std::cout << "Cannot read the header.\n";
 
     samFile *out = sam_open(outputFile, "wb");
-    if (!out) std::cout << "Ne mogu stvoriti bam datoteku header.\n";
+    if (!out) std::cout << "Cannot make the BAM file header.\n";
 
     if (sam_hdr_write(out, header) < 0) 
-        std::cout << "Ne mogu pisati u bam datoteku.\n";
+        std::cout << "Cannot write in the BAM file:.\n";
 
 
-    // Prazna bam1 struktura
+    // Empty BAM1 structure
     bam1_t *aln = bam_init1();
 
-    // Čitanje datoteke
+    // Reading the file
     while (sam_read1(file, header, aln) >= 0) {
         if (sam_write1(out, header, aln) < 0) {
-            std::cerr << "Greška pri pisanju zapisa.\n";
+            std::cerr << "Error writing record.\n";
             break;
         }
     }
 
     /*
-        Čišćenje memorije
+        Memory cleanup
     */
 
     bam_destroy1(aln);
@@ -56,10 +52,11 @@ void convert_sam_to_bam(const char* inputFile, const char* outputFile) {
     sam_close(file);
     sam_close(out);
 
-    std::cout << "Konverzija uspješno završena!" << std::endl;
+    std::cout << "Conversion completed successfully!" << std::endl;
 }
 /*
-    pomoćna funkcija : reverse_complement
+    Helper function: reverse_complement swapping base pairs
+    Written by: Ivan Dundović
 */
 std::string reverse_complement(std::string seq) {
     std::map<char,char> comp = {{'A','T'},{'T','A'},{'C','G'},{'G','C'}};
@@ -69,25 +66,27 @@ std::string reverse_complement(std::string seq) {
     return temp;
 }
 /*
-    parser sam-a, ako bude potrebno možemo dodavati još vrijednosti koje treba parsirati
+    SAM parser
+    Written by: Ivan Dundović
 */
 std::vector<SamRecord> parse_sam(std::string filename){
     std::vector<SamRecord> records;
     std::ifstream file(filename);
-    if(!file.is_open()) throw std::runtime_error("Ne mogu otvoriti fajl: " + filename);
+    if(!file.is_open()) throw std::runtime_error("Cannot open the file:  " + filename);
     std::string line;
     while (getline(file, line)){
         if(line[0] == '@') continue;
-
+        // Splitting lines into columns
         std::stringstream ss(line);
         std::string token;
         std::vector<std::string> columns;
         while (ss >> token){
             columns.push_back(token);
         }
+        // Saving selected columns into SAM records
         SamRecord record;
         record.flag = std::stoi(columns[1]);
-        record.pos = std::stoi(columns[3]) - 1; //  SAM je 1-based, pa zato -1 da imamo 0-based
+        record.pos = std::stoi(columns[3]) - 1; // SAM is 1-based, so we subtract 1 to convert to 0-based indexing
         record.cigar = columns[5];
         record.seq = columns[9];
 
